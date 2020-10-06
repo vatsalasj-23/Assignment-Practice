5f228adf7a74e6ce789ea5a4b09ffb5896bd3f52
vatsalasj-23 <72140816+vatsalasj-23@users.noreply.github.com>
2020-10-07 00:06:54 +0530

    Add files via upload

---
+++ CGenerics.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sharpGenerics
+{
+	class student<T>
+	{
+		T data;             //type parameter,method for a class
+		public student(T value)
+		{
+			this.data = value;
+
+		}
+		public void Write()
+		{
+			Console.WriteLine("The passed initializer is " + this.data);
+			Console.ReadLine();
+
+		}
+	}
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			student<string> Name = new student<string>("DARSHAN");
+			student<int> age = new student<int>(15);
+			student<bool> Present = new student<bool>(true);
+
+			Name.Write();
+			age.Write();
+			Present.Write();
+
+
+		}
+	}
+}
