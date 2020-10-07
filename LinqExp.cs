99b734511580b8fd793f3c7ccd2de343a94947a7
vatsalasj-23 <72140816+vatsalasj-23@users.noreply.github.com>
2020-10-07 17:10:03 +0530

    Add files via upload

---
+++ LinqExp.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CLinq1
+{
+	class LinqExp
+	{
+		static void Main(string[] args)
+		{
+			string[] employee = { "vatsala", "darshan", "priya", "hari", "jagadeesha", "lalitha", "ganga" };
+
+			var emp = from V in employee where V.Length > 6 orderby V descending select V;
+
+			foreach (string x in emp)
+			{
+				Console.WriteLine(x+" ");
+				Console.ReadLine();
+			}
+			Console.WriteLine("*******************************************************************");
+			var sgh = from X in employee where X.Length < 5 orderby X ascending select X;
+			foreach (string y in sgh)
+			{
+				Console.WriteLine(y+" ");
+				Console.ReadLine();
+			}
+
+
+
+		}
+	}
+
+}
---
+++ LinqPg.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CLinqSharp
+{
+	class LinqPg
+	{
+		static void Main(string[] args)
+		{
+			int[] array = { 11, 21, 34, 4, 54, 55, 67, 80, 90, 99, 123, 32, 34, 35 };
+
+			var Num = from i in array where i > 30 orderby i descending select i;
+			foreach (int x in Num)
+			{
+				Console.WriteLine(x);
+				Console.ReadLine();
+
+			}
+			Console.WriteLine("\n");
+
+			var number = from g in array where g < 30 orderby g ascending select g;
+			foreach (int y in number)
+			{
+				Console.WriteLine(y);
+				Console.ReadLine();
+
+			}
+			Console.WriteLine();
+			Console.WriteLine("\n");
+			Console.WriteLine("*******************************************************************************");
+
+			string[] student = { "jagadeesha", "lalitha", "darshan", "vatsala", "ganga", "pavi" };
+
+			var studentList = from S in student where S.Length > 6 orderby S descending select S;
+
+			foreach (string n in studentList)
+			{
+				Console.WriteLine(n);
+				Console.ReadLine();
+
+			}
+
+			Console.WriteLine("\n");
+
+			var Studentss = from v in student where v.Length < 6 orderby v ascending select v;
+			foreach (string j in Studentss)
+			{
+				Console.WriteLine(j);
+				Console.ReadLine();
+			}
+
+
+
+		}
+	}
+}
