9e48a78d5523f180ddc01a1c9e9552a548b1e33f
vatsalasj-23 <72140816+vatsalasj-23@users.noreply.github.com>
2020-10-15 00:53:44 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,32 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ANonymousDel
+namespace html4
 {
-	public delegate int AddnumsDelegate(int a, int b, int c);
-
 	class Program
 	{
-		//public static int Addnums(int a, int b, int c)
-		//{
-
-		//	Console.WriteLine(a+b+c);
-
-		//}
 		static void Main(string[] args)
 		{
-			//AddnumsDelegate add = new AddnumsDelegate(Addnums);
-			
-			AddnumsDelegate add = delegate (int a, int b, int c)
-			{
-				Console.WriteLine(a + b + c);
-			};
-			int v=add.Invoke(100, 50, 150);
-			Console.WriteLine(v);
-			Console.ReadLine();
-
-
-
 		}
 	}
 }
