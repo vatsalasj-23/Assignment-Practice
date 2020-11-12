a683ca2a6514de7e0f2961371ba6d3603225ab5a
vatsalasj-23 <72140816+vatsalasj-23@users.noreply.github.com>
2020-11-13 00:33:19 +0530

    Add files via upload

---
+++ 12_NovAssignment/ActionLink/FirstController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ActionLink.Models;
+
+namespace ActionLink.Controllers
+{
+    public class FirstController : Controller
+
+    {
+        // GET: First
+        public ActionResult Index()
+        {
+			Employee emp = new Employee()//Instance for the model class- employee
+			{
+				Name = "Vatsala sj",
+				Salary = "davanagere",
+				DepartmentName ="Engineer"
+			};
+
+            return View();
+        }
+		[HttpPost]
+		public ActionResult Index(Employee emp)
+		{
+			return View();
+		}
+    }
+}
